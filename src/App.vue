d759f8ab139f965f04376496a1e1942b3065c184
husanr <i2y521@yeah.net>
2024-02-05 17:28:21 +0800

    chore: 配置vue-router路由跳转

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>app</div>
+  <div>
+    <h3>app</h3>
+    <router-link to="/login">去登录</router-link>
+    <router-link to="/main">去主页</router-link>
+    <router-view></router-view>
+  </div>
 </template>
 <script setup lang="ts"></script>
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
 import 'normalize.css'
 import '@/assets/css/index.less'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
