9b1f05a3f5d98cd3d3544afa986e613fb54cb27c
husanr <i2y521@yeah.net>
2024-02-05 18:02:16 +0800

    chore: 配置pinia并使用

--- src/views/main/Main.vue
+++ src/views/main/Main.vue
@@ -1,8 +1,20 @@
 <template>
-  <div class="main">main</div>
+  <div class="main">
+    <h3>Main</h3>
+    <h4>{{ counterStore.counter }} --- {{ counterStore.doubleCounter }}</h4>
+    <button @click="changeCounter">改变counter</button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import useCounterStore from '@/store/counter'
+
+const counterStore = useCounterStore()
+
+function changeCounter() {
+  counterStore.changeCounter(999)
+}
+</script>
 
 <style scoped lang="less">
 .main {
